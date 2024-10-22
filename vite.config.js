8fc3f4c2670bbf064d52aa60ab11972d7d8656fa
Prashant Singh <prashantsh7014@gmail.com>
2024-10-22 13:51:42 +0530

    update folder

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  build: {
-    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit (in KB)
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-  },
+  base: "/", // YOUR REPO NAME HERE
 });
